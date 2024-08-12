1bbc75ce253837862715c29825dab576fa7390aa
oussama-hadda <75843673+oussama-hadda@users.noreply.github.com>
2024-08-12 03:59:07 +0200

    :sparkles: add server

--- API/server.ts
+++ API/server.ts
@@ -1,9 +1,48 @@
-import sequelize from "./config/database";
+import express, { Request, Response } from 'express';
+import sequelize from './config/database';
+import userRoutes from "./routes/userRoutes";
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch((err: Error) => {
-        console.error('Unable to connect to the database:', err);
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/api', userRoutes);
+
+// app.get('/', async (req: Request, res: Response) => {
+//     try {
+//         // Test connection to database
+//         await sequelize.authenticate();
+//         console.log('Database connection has been established successfully.');
+//
+//         const users = await User.findAll();
+//         res.json(users);
+//         console.log('Users:');
+//         console.log(users);
+//
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//         res.status(500).json({ error: 'Unable to connect to the database' });
+//     }
+// });
+
+
+// (async () => {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Database connection established.');
+//
+//         await User.drop();
+//         console.log('User table dropped!');
+//
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+// })();
+
+sequelize.sync({ force: true }).then(() => {
+    console.log('Database synced');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
+});
